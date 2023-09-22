c60d8cd42ba8e834ef9e4a409f9041982dcc7308
EsraelSousa <esraelsousa22@gmail.com>
2023-09-22 00:31:21 -0300

    add rv, ma, mp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <fstream>
 #include <string.h>
+#include <math.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -70,13 +72,29 @@ vector<vector<int>> listaAdjacencia2matrizAdjacencia(vector<vector<edge>> &lista
         exit(1);
     }
     int n = listaAdjacencia.size();
-    vector<vector<int>> matrizAdjacencia(n+1, vector<int>(n+1, 0)); // cria a matriz e seta com 0's
+    vector<vector<int>> matrizAdjacencia(n, vector<int>(n, 0)); // cria a matriz e seta com 0's
     for(int i=1; i<listaAdjacencia.size(); i++){
         for(auto &par: listaAdjacencia[i])
             matrizAdjacencia[i][ par.first ] = 1;
     }
     return matrizAdjacencia;
 }
+
+vector<vector<double>> listaAdjacencia2matrizPesos(vector<vector<edge>> &listaAdjacencia){
+    // não é possivel fazer matriz nesse programa com mais de 10^6 celulas
+    if(listaAdjacencia.size() > 1000){
+        cout << "O grafo tem muitos vertices, nao eh possivel exibir '-'\n";
+        exit(1);
+    }
+    int n = listaAdjacencia.size();
+    vector<vector<double>> matrizAdjacencia(n, vector<double>(n, 0)); // cria a matriz e seta com 0's
+    for(int i=1; i<listaAdjacencia.size(); i++){
+        for(auto &par: listaAdjacencia[i])
+            matrizAdjacencia[i][ par.first ] = par.second;
+    }
+    return matrizAdjacencia;
+}
+
 
 void showGrafoListaAdjacencia(vector<vector<edge>> &listaAdjacencia){
     for(int i=1; i < (int)listaAdjacencia.size(); i++){
@@ -91,12 +109,97 @@ void showGrafoListaAdjacencia(vector<vector<edge>> &listaAdjacencia){
     }
 }
 
+int countDigitos(int n){
+    int ans = 1;
+    while(n > 9){
+        ans++;
+        n /= 10;
+    }
+    return ans;
+}
+
+void showGrafoMatrizAdjacencia(vector<vector<int>> &matrizAdjacencia){
+    int n = matrizAdjacencia.size();
+    int qtdDigitos = countDigitos(n-1);
+    for(int i=1; i<n; i++){
+        if(i == 1)
+            // um espaço para colocar os vertices na primeira coluna
+            cout << "\033[1;32m" << setw(qtdDigitos+2) << setfill(' ') << i << "\033[0m" << ' ';
+        else
+            cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << i << "\033[0m" << (i+1 == n? '\n' : ' ');
+    }
+    for(int i=1; i<n; i++){
+        cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << i << "\033[0m" << ' ';
+        for(int j=1; j<n; j++)
+            cout << setw(qtdDigitos) << setfill(' ') << matrizAdjacencia[i][j] << (j+1 == n? '\n' : ' ');
+    }
+}
+
+void showGrafoMatrizPesos(vector<vector<double>> &matrizPesos){
+    int n = matrizPesos.size();
+    int qtdDigitos = countDigitos(n-1)+2;
+    // vamos ver o maior pesso para formatar a saida
+    for(int i=1; i<n; i++)
+        for(int j=1; j<n; j++)
+            qtdDigitos = max(qtdDigitos, countDigitos(matrizPesos[i][j]));
+    for(int i=1; i<n; i++){
+        if(i == 1)
+            // um espaço para colocar os vertices na primeira coluna
+            cout << "\033[1;32m" << setw(qtdDigitos+2) << setfill(' ') << i << "\033[0m" << ' ';
+        else
+            cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << i << "\033[0m" << (i+1 == n? '\n' : ' ');
+    }
+    for(int i=1; i<n; i++){
+        cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << i << "\033[0m" << ' ';
+        for(int j=1; j<n; j++)
+            cout << setw(qtdDigitos) << setfill(' ') << setprecision(2) << matrizPesos[i][j] << (j+1 == n? '\n' : ' ');
+    }
+}
+
+/*Não vamos usar os vetores em si, mas se aproveitar da lista de adjacencia,
+  já que essa representação é a concatenação das listas*/
+void showGrafoRepresentacaoVetorial(vector<vector<edge>> &listaAdjacencia){
+    int n = listaAdjacencia.size();
+    vector<int> qtdEspacos(n, 0); // todas as posições com 0
+    // vamos contabilizar os espaços necessários para formatar
+    for(int i=1; i<n; i++){
+        for(auto &par: listaAdjacencia[i])
+            qtdEspacos[i] += countDigitos(par.first)+1;
+        qtdEspacos[i] -= 1; // retira um espaço a mais que tem
+    }
+    // 1º vetor
+    for(int i=1; i < n; i++){
+        if(i & 1){ // é impar
+            cout << "\033[1;32m" << setw(qtdEspacos[i]) << setfill(' ') << listaAdjacencia[i].size() << "\033[0m" << (i+1 == n? '\n' : ' ');
+        }
+        else{
+            cout << "\033[1;34m" << setw(qtdEspacos[i]) << setfill(' ') << listaAdjacencia[i].size() << "\033[0m" << (i+1 == n? '\n' : ' ');
+        }
+    }
+    // 2º vetor
+    for(int i=1; i < n; i++){
+        bool isFirst = 1;
+        for(auto &par: listaAdjacencia[i]){
+            if(!isFirst) cout << ' ';
+            if(i & 1){ // é impar
+                cout << "\033[1;32m" << par.first << "\033[0m";
+            }
+            else{
+                cout << "\033[1;34m" << par.first << "\033[0m";
+            }
+            isFirst = 0;
+        }
+        cout << (i+1 == n? '\n' : ' ');
+    }
+}
+
+
 int main(int argc, char* argv[]){
     ios_base::sync_with_stdio(0); cin.tie(nullptr); // faz uma otimização de leitura
     // checar se são fornecidos todos os argumentos
     if (argc < 6) {
         cerr << "\033[1;31m Para executar esse programa use:\033[0m " << argv[0] <<
-        " read inst-10 show -la 0\n"; return 1;
+        " <read> <nomeTeste(inst-*)> <show> <estrutura(-[ma, mi, la, rv, mp])> <isDirecionado(0/1)>\n"; return 1;
     }
     // pegar argumentos importantes
     string nomeInput = argv[2];
@@ -129,6 +232,21 @@ int main(int argc, char* argv[]){
     }
     // processar os comandos
     vector<vector<edge>> listaAdjacencia = readGrafo(nomeArquivo, isDirecionado);
-    showGrafoListaAdjacencia(listaAdjacencia);
+    if(tipoEstrutura == "-la")
+        showGrafoListaAdjacencia(listaAdjacencia);
+    else if(tipoEstrutura == "-ma"){
+        vector<vector<int>> matrizAdjacencia = listaAdjacencia2matrizAdjacencia(listaAdjacencia);
+        showGrafoMatrizAdjacencia(matrizAdjacencia);
+    }
+    else if(tipoEstrutura == "-mi"){
+
+    }
+    else if(tipoEstrutura == "-rv"){
+        showGrafoRepresentacaoVetorial(listaAdjacencia);
+    }
+    else if(tipoEstrutura == "-mp"){
+        vector<vector<double>> matrizPesos = listaAdjacencia2matrizPesos(listaAdjacencia);
+        showGrafoMatrizPesos(matrizPesos);
+    }
     return 0;
 }
