df65ed6b4e215e498b0179dc6546c872e552ccdd
EsraelSousa <esraelsousa22@gmail.com>
2023-09-22 18:43:16 -0300

    finalizado

--- main.cpp
+++ main.cpp
@@ -11,41 +11,6 @@ using namespace std;
 // vamos definir uma estrutura para representar as arestas usando o pair do C++
 typedef pair<int, double> edge;
 
-vector<vector<edge>> readGrafo(string &nomeArquivo, bool isDirecionado){
-    // Para facilitar, vamos redirecionar o buffer de entrada para o arquivo
-    
-    ifstream arquivoInput(nomeArquivo);
-    if(arquivoInput.is_open() == false){
-        cout << "Nao foi possivel abrir o arquivo\n";
-        exit(1);
-    }
-    // Salva o estado atual da entrada padrão (cin)
-    streambuf* originalCin = std::cin.rdbuf();
-
-    // Redireciona a entrada padrão para o arquivo
-    cin.rdbuf(arquivoInput.rdbuf());
-    
-    int n; // numero de vertices
-    cin >> n;
-    vector<vector<edge>> listaAdjacencia(n+1); // precizamos de +1, pois é indexado em 0.
-    // vamos fazer a leitura do grafo
-    int u, v; // vertices
-    double peso;
-    while(cin >> u >> v >> peso){ // enquanto não chegar no EOF
-        listaAdjacencia[u].push_back(make_pair(v, peso));
-        if(isDirecionado == false)
-            listaAdjacencia[v].push_back(make_pair(u, peso));
-    }
-
-    // Restaura o estado original da entrada padrão
-    cin.rdbuf(originalCin);
-
-    // Fecha o arquivo após a leitura
-    arquivoInput.close();
-
-    return listaAdjacencia;
-}
-
 bool isUmaEstruturaValida(string &s){
     if(s == "-la") return 1;
     if(s == "-ma") return 1;
@@ -66,7 +31,9 @@ void showComandosEstruturas(){
 }
 
 void checkPossivelConverterMatrriz(vector<vector<edge>> &listaAdjacencia){
-    // não é possivel fazer matriz nesse programa com mais de 10^6 celulas
+    /* não é possivel fazer matriz nesse programa com mais de 10^6 celulas, para matriz
+       vai passar um pouco, mas se o grafo for direcionado e completo não vai dar para 
+       alocar espaço na memoria, pois precisa O(9 * 10^8) de memoria [(1000 * 999) * 1000] */ 
     if(listaAdjacencia.size() > 1000){
         cout << "O grafo tem muitos vertices, nao eh possivel exibir '-'\n";
         exit(1);
@@ -74,10 +41,65 @@ void checkPossivelConverterMatrriz(vector<vector<edge>> &listaAdjacencia){
     // se não entrou no if, continua o programa
 }
 
+template <typename T> /* isso vai deixar generico para qualquer tipo, assim não faz uma função 
+                        para o tipo int e outra para o tipo double */
+void checkAlocouMemoria(vector<vector< T >> &variavel){
+    if(variavel.empty()){
+        cout << "Nao foi possivel alocar memoria! '-'\n";
+        exit(1);
+    }
+}
+
+int countDigitos(int n){
+    int ans = 1;
+    while(n > 9){
+        ans++;
+        n /= 10;
+    }
+    return ans;
+}
+
+vector<vector<edge>> readGrafo(string &nomeArquivo, bool isDirecionado){
+    // Para facilitar, vamos redirecionar o buffer de entrada para o arquivo
+    
+    ifstream arquivoInput(nomeArquivo);
+    if(arquivoInput.is_open() == false){
+        cout << "Nao foi possivel abrir o arquivo\n";
+        exit(1);
+    }
+    // Salva o estado atual da entrada padrão (cin)
+    streambuf* originalCin = std::cin.rdbuf();
+
+    // Redireciona a entrada padrão para o arquivo
+    cin.rdbuf(arquivoInput.rdbuf());
+    
+    int n; // numero de vertices
+    cin >> n;
+    vector<vector<edge>> listaAdjacencia(n+1); // precizamos de +1, pois é indexado em 0.
+    checkAlocouMemoria(listaAdjacencia);
+    // vamos fazer a leitura do grafo
+    int u, v; // vertices
+    double peso;
+    while(cin >> u >> v >> peso){ // enquanto não chegar no EOF
+        listaAdjacencia[u].push_back(make_pair(v, peso));
+        if(isDirecionado == false)
+            listaAdjacencia[v].push_back(make_pair(u, peso));
+    }
+
+    // Restaura o estado original da entrada padrão
+    cin.rdbuf(originalCin);
+
+    // Fecha o arquivo após a leitura
+    arquivoInput.close();
+
+    return listaAdjacencia;
+}
+
 vector<vector<int>> listaAdjacencia2matrizAdjacencia(vector<vector<edge>> &listaAdjacencia){
     checkPossivelConverterMatrriz(listaAdjacencia);
     int n = listaAdjacencia.size();
     vector<vector<int>> matrizAdjacencia(n, vector<int>(n, 0)); // cria a matriz e seta com 0's
+    checkAlocouMemoria(matrizAdjacencia);
     for(int i=1; i<listaAdjacencia.size(); i++){
         for(auto &par: listaAdjacencia[i])
             matrizAdjacencia[i][ par.first ] = 1;
@@ -89,6 +111,7 @@ vector<vector<double>> listaAdjacencia2matrizPesos(vector<vector<edge>> &listaAd
     checkPossivelConverterMatrriz(listaAdjacencia);
     int n = listaAdjacencia.size();
     vector<vector<double>> matrizAdjacencia(n, vector<double>(n, 0)); // cria a matriz e seta com 0's
+    checkAlocouMemoria(matrizAdjacencia);
     for(int i=1; i<listaAdjacencia.size(); i++){
         for(auto &par: listaAdjacencia[i])
             matrizAdjacencia[i][ par.first ] = par.second;
@@ -98,24 +121,25 @@ vector<vector<double>> listaAdjacencia2matrizPesos(vector<vector<edge>> &listaAd
 
 vector<vector<int>> listaAdjacencia2matrizIncidencia(vector<vector<edge>> &listaAdjacencia, bool isDirecionado){
     checkPossivelConverterMatrriz(listaAdjacencia);
-    /* A matriz de incidencias mostra que as linhas são os vertices e as colunas são as arestas.
-    Se a aresta não está relacionada ao vertice v_i, então o valor da célula é 0, mas se estiver 
-    relacionada o valor da célula vai ser 1 se a aresta chega aquele vertice e -1 se ela sai daquele 
-    vertice. Então podemos concluir que, se o grafo não é direcionado a matriz de incidencias é
-    equivalente a matriz de adjacencias */
-    if(isDirecionado == false)
-        return listaAdjacencia2matrizAdjacencia(listaAdjacencia); // retornamos a matriz de adjacencia
     int n = listaAdjacencia.size(); // numero de vertices
     int m = 0; // numero de arestas
     int idAresta = 0;
     // contamos quantas arestas existem no grafo
     for(int i=1; i<n; i++)
         m += listaAdjacencia[i].size();
+    if(isDirecionado == false) // estamos contando cada aresta duas vezes
+        m /= 2;
     vector<vector<int>> matrizIncidencia(n, vector<int>(m, 0)); // as arestas serão indexadas de 0 a m-1
     for(int i=1; i<n; i++){
         for(auto &par: listaAdjacencia[i]){
-            matrizIncidencia[i][idAresta] = -1;
-            matrizIncidencia[par.first][idAresta] = 1;
+            if(isDirecionado){
+                matrizIncidencia[i][idAresta] = -1;
+                matrizIncidencia[par.first][idAresta] = 1;
+            }
+            else{ // cada aresta sai, mas também chega.
+                matrizIncidencia[i][idAresta] = 1;
+                matrizIncidencia[par.first][idAresta] = 1;
+            }
             idAresta++;
         }
     }
@@ -128,20 +152,11 @@ void showGrafoListaAdjacencia(vector<vector<edge>> &listaAdjacencia){
         bool isFirst = 1;
         for(auto &par: listaAdjacencia[i]){
             if(!isFirst) cout << ", ";
-            cout << "{" << par.first << ", " << par.second << "}";
+            cout << "{" << par.first << ", " << fixed << setprecision(2) << par.second << "}";
             isFirst = 0;
         }
         cout << "\n";
     }
-}
-
-int countDigitos(int n){
-    int ans = 1;
-    while(n > 9){
-        ans++;
-        n /= 10;
-    }
-    return ans;
 }
 
 void showGrafoMatrizAdjacencia(vector<vector<int>> &matrizAdjacencia){
@@ -163,22 +178,22 @@ void showGrafoMatrizAdjacencia(vector<vector<int>> &matrizAdjacencia){
 
 void showGrafoMatrizPesos(vector<vector<double>> &matrizPesos){
     int n = matrizPesos.size();
-    int qtdDigitos = countDigitos(n-1)+2;
+    int qtdDigitos = countDigitos(n-1);
     // vamos ver o maior pesso para formatar a saida
     for(int i=1; i<n; i++)
         for(int j=1; j<n; j++)
-            qtdDigitos = max(qtdDigitos, countDigitos(matrizPesos[i][j]));
+            qtdDigitos = max(qtdDigitos, countDigitos(matrizPesos[i][j])+3);
     for(int i=1; i<n; i++){
         if(i == 1)
             // um espaço para colocar os vertices na primeira coluna
-            cout << "\033[1;32m" << setw(qtdDigitos+2) << setfill(' ') << i << "\033[0m" << ' ';
+            cout << "\033[1;32m" << setw(qtdDigitos+2) << setfill(' ') << right << i << "\033[0m" << ' ';
         else
-            cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << i << "\033[0m" << (i+1 == n? '\n' : ' ');
+            cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << right << i << "\033[0m" << (i+1 == n? '\n' : ' ');
     }
     for(int i=1; i<n; i++){
-        cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << i << "\033[0m" << ' ';
+        cout << "\033[1;32m" << setw(qtdDigitos) << setfill(' ') << left << i << "\033[0m" << ' ';
         for(int j=1; j<n; j++)
-            cout << setw(qtdDigitos) << setfill(' ') << setprecision(2) << matrizPesos[i][j] << (j+1 == n? '\n' : ' ');
+            cout << setw(qtdDigitos) << setfill(' ') << left << fixed << setprecision(2) << matrizPesos[i][j] << (j+1 == n? '\n' : ' ');
     }
 }
 
@@ -188,20 +203,18 @@ void showGrafoMatrizIncidencia(vector<vector<int>> &matrizIncidencia){
     // pegamos o maximo de digitos que precisamos para representar um numero, (2 devido ao -1)
     int qtdEspacos = max(2, max(countDigitos(n-1), countDigitos(m-1)));
     // identificação das arestas
-    for(int i=0; i<m; i++)
-        cout << setw(qtdEspacos) << setfill(' ') << "\033[1;34m" << i << "\033[0m" << (i+1 == m? '\n' : ' ');
+    for(int i=0; i<m; i++){
+        if(i == 0)
+            // um espaço para colocar os vertices na primeira coluna
+            cout << "\033[1;34m" << setw(qtdEspacos+2) << setfill(' ') << right << i << "\033[0m" << ' ';
+        else
+            cout << "\033[1;34m" << setw(qtdEspacos) << setfill(' ') << right << i << "\033[0m" << (i+1 == m? '\n' : ' ');
+    }
     for(int i=1; i<n; i++){
-        cout << setw(qtdEspacos) << setfill(' ') << "\033[1;34m" << i << " \033[0m";
+        cout << setw(qtdEspacos) << setfill(' ') << left << "\033[1;34m" << i << " \033[0m";
+        /*Não foi possivel usar cores diferentes para -1, 1 e 0. O uso de cores bagunçava a formatação '-' */
         for(int j=0; j<m; j++){
-            if(matrizIncidencia[i][j] == -1){
-                cout << setw(qtdEspacos) << setfill(' ') << "\033[1;31m" << -1 << "\033[0m" << (j+1 == m? '\n' : ' ');
-            }
-            else if(matrizIncidencia[i][j] == 1){
-                cout << setw(qtdEspacos) << setfill(' ') << "\033[1;32m" << 1 << "\033[0m" << (j+1 == m? '\n' : ' ');
-            }
-            else{
-                cout << setw(qtdEspacos) << setfill(' ') << 0 << (j+1 == m? '\n' : ' ');
-            }
+            cout << setw(qtdEspacos) << setfill(' ') << right << matrizIncidencia[i][j] << ((j+1 == m)? '\n' : ' ');
         }
     }
 }
