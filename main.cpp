1fdf8780da0ec816b7a67f687f22873be9fae5a9
EsraelSousa <esraelsousa22@gmail.com>
2023-09-22 14:23:07 -0300

    no geral está pronto, mas falta corrigir alguns problemas nas formatações das saida de algumas matrizes

--- main.cpp
+++ main.cpp
@@ -65,12 +65,17 @@ void showComandosEstruturas(){
     cout << "\033[1;34m  -mp    -> matriz de pesos\n\033[0m";
 }
 
-vector<vector<int>> listaAdjacencia2matrizAdjacencia(vector<vector<edge>> &listaAdjacencia){
+void checkPossivelConverterMatrriz(vector<vector<edge>> &listaAdjacencia){
     // não é possivel fazer matriz nesse programa com mais de 10^6 celulas
     if(listaAdjacencia.size() > 1000){
         cout << "O grafo tem muitos vertices, nao eh possivel exibir '-'\n";
         exit(1);
     }
+    // se não entrou no if, continua o programa
+}
+
+vector<vector<int>> listaAdjacencia2matrizAdjacencia(vector<vector<edge>> &listaAdjacencia){
+    checkPossivelConverterMatrriz(listaAdjacencia);
     int n = listaAdjacencia.size();
     vector<vector<int>> matrizAdjacencia(n, vector<int>(n, 0)); // cria a matriz e seta com 0's
     for(int i=1; i<listaAdjacencia.size(); i++){
@@ -81,11 +86,7 @@ vector<vector<int>> listaAdjacencia2matrizAdjacencia(vector<vector<edge>> &lista
 }
 
 vector<vector<double>> listaAdjacencia2matrizPesos(vector<vector<edge>> &listaAdjacencia){
-    // não é possivel fazer matriz nesse programa com mais de 10^6 celulas
-    if(listaAdjacencia.size() > 1000){
-        cout << "O grafo tem muitos vertices, nao eh possivel exibir '-'\n";
-        exit(1);
-    }
+    checkPossivelConverterMatrriz(listaAdjacencia);
     int n = listaAdjacencia.size();
     vector<vector<double>> matrizAdjacencia(n, vector<double>(n, 0)); // cria a matriz e seta com 0's
     for(int i=1; i<listaAdjacencia.size(); i++){
@@ -95,6 +96,31 @@ vector<vector<double>> listaAdjacencia2matrizPesos(vector<vector<edge>> &listaAd
     return matrizAdjacencia;
 }
 
+vector<vector<int>> listaAdjacencia2matrizIncidencia(vector<vector<edge>> &listaAdjacencia, bool isDirecionado){
+    checkPossivelConverterMatrriz(listaAdjacencia);
+    /* A matriz de incidencias mostra que as linhas são os vertices e as colunas são as arestas.
+    Se a aresta não está relacionada ao vertice v_i, então o valor da célula é 0, mas se estiver 
+    relacionada o valor da célula vai ser 1 se a aresta chega aquele vertice e -1 se ela sai daquele 
+    vertice. Então podemos concluir que, se o grafo não é direcionado a matriz de incidencias é
+    equivalente a matriz de adjacencias */
+    if(isDirecionado == false)
+        return listaAdjacencia2matrizAdjacencia(listaAdjacencia); // retornamos a matriz de adjacencia
+    int n = listaAdjacencia.size(); // numero de vertices
+    int m = 0; // numero de arestas
+    int idAresta = 0;
+    // contamos quantas arestas existem no grafo
+    for(int i=1; i<n; i++)
+        m += listaAdjacencia[i].size();
+    vector<vector<int>> matrizIncidencia(n, vector<int>(m, 0)); // as arestas serão indexadas de 0 a m-1
+    for(int i=1; i<n; i++){
+        for(auto &par: listaAdjacencia[i]){
+            matrizIncidencia[i][idAresta] = -1;
+            matrizIncidencia[par.first][idAresta] = 1;
+            idAresta++;
+        }
+    }
+    return matrizIncidencia;
+}
 
 void showGrafoListaAdjacencia(vector<vector<edge>> &listaAdjacencia){
     for(int i=1; i < (int)listaAdjacencia.size(); i++){
@@ -156,9 +182,33 @@ void showGrafoMatrizPesos(vector<vector<double>> &matrizPesos){
     }
 }
 
-/*Não vamos usar os vetores em si, mas se aproveitar da lista de adjacencia,
-  já que essa representação é a concatenação das listas*/
+void showGrafoMatrizIncidencia(vector<vector<int>> &matrizIncidencia){
+    int n = matrizIncidencia.size(); // numero vertices
+    int m = matrizIncidencia[1].size(); // numeros arestas
+    // pegamos o maximo de digitos que precisamos para representar um numero, (2 devido ao -1)
+    int qtdEspacos = max(2, max(countDigitos(n-1), countDigitos(m-1)));
+    // identificação das arestas
+    for(int i=0; i<m; i++)
+        cout << setw(qtdEspacos) << setfill(' ') << "\033[1;34m" << i << "\033[0m" << (i+1 == m? '\n' : ' ');
+    for(int i=1; i<n; i++){
+        cout << setw(qtdEspacos) << setfill(' ') << "\033[1;34m" << i << " \033[0m";
+        for(int j=0; j<m; j++){
+            if(matrizIncidencia[i][j] == -1){
+                cout << setw(qtdEspacos) << setfill(' ') << "\033[1;31m" << -1 << "\033[0m" << (j+1 == m? '\n' : ' ');
+            }
+            else if(matrizIncidencia[i][j] == 1){
+                cout << setw(qtdEspacos) << setfill(' ') << "\033[1;32m" << 1 << "\033[0m" << (j+1 == m? '\n' : ' ');
+            }
+            else{
+                cout << setw(qtdEspacos) << setfill(' ') << 0 << (j+1 == m? '\n' : ' ');
+            }
+        }
+    }
+}
+
 void showGrafoRepresentacaoVetorial(vector<vector<edge>> &listaAdjacencia){
+    /*Não vamos usar os vetores em si, mas se aproveitar da lista de adjacencia,
+    já que essa representação é a concatenação das listas*/
     int n = listaAdjacencia.size();
     vector<int> qtdEspacos(n, 0); // todas as posições com 0
     // vamos contabilizar os espaços necessários para formatar
@@ -166,6 +216,7 @@ void showGrafoRepresentacaoVetorial(vector<vector<edge>> &listaAdjacencia){
         for(auto &par: listaAdjacencia[i])
             qtdEspacos[i] += countDigitos(par.first)+1;
         qtdEspacos[i] -= 1; // retira um espaço a mais que tem
+        
     }
     // 1º vetor
     for(int i=1; i < n; i++){
@@ -189,6 +240,7 @@ void showGrafoRepresentacaoVetorial(vector<vector<edge>> &listaAdjacencia){
             }
             isFirst = 0;
         }
+        if(listaAdjacencia[i].size() == 0) cout << ' '; // corrigir espaços para vertices sem vizinhos
         cout << (i+1 == n? '\n' : ' ');
     }
 }
@@ -197,7 +249,7 @@ void showGrafoRepresentacaoVetorial(vector<vector<edge>> &listaAdjacencia){
 int main(int argc, char* argv[]){
     ios_base::sync_with_stdio(0); cin.tie(nullptr); // faz uma otimização de leitura
     // checar se são fornecidos todos os argumentos
-    if (argc < 6) {
+    if (argc < 6 || argc > 6) {
         cerr << "\033[1;31m Para executar esse programa use:\033[0m " << argv[0] <<
         " <read> <nomeTeste(inst-*)> <show> <estrutura(-[ma, mi, la, rv, mp])> <isDirecionado(0/1)>\n"; return 1;
     }
@@ -239,7 +291,8 @@ int main(int argc, char* argv[]){
         showGrafoMatrizAdjacencia(matrizAdjacencia);
     }
     else if(tipoEstrutura == "-mi"){
-
+        vector<vector<int>> matrizIncidencia = listaAdjacencia2matrizIncidencia(listaAdjacencia, isDirecionado);
+        showGrafoMatrizIncidencia(matrizIncidencia);
     }
     else if(tipoEstrutura == "-rv"){
         showGrafoRepresentacaoVetorial(listaAdjacencia);
